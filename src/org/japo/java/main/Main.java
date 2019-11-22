07d3a2f16912e85b031996a89b68a6185b8a8c7a
Andrés Cosín Zamarreño <Cousin183@gmail.com>
2019-11-22 13:34:45 +0100

    primer commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,58 @@
+/* 
+ * Copyright 2019 ANDRÉS COSÍN ZAMARREÑO - Cousin183@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ANDRÉS COSÍN ZAMARREÑO - Cousin183@gmail.com
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        double numReal;
+        boolean todoOK = true;
+
+        do {
+            try {
+                System.out.print("Introduce un número real que esté aprobado..: ");
+                numReal = SCN.nextDouble();
+                boolean aprobado = numReal >= 5 && numReal <= 10;
+
+                if (aprobado) {
+                    System.out.printf("El alumno ha APROBADO con ...: %.02f%n", numReal);
+                    todoOK = false;
+                } else {
+                    System.out.println("Esta nota está fuera de los valores del aprobado, vuelve a intentarlo.");
+
+                }
+
+            } catch (Exception e) {
+                System.out.println("ERROR DE ENTRADA");
+            } finally {
+                SCN.nextLine();
+            }
+        } while (todoOK);
+
+    }
+
+}
